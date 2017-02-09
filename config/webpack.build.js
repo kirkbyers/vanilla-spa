c10b408e1626f59d8d38d06f965c6fa715999164
Kirk byers <kingbyers@gmail.com>
2017-02-08 22:38:26 -0600

    Add compression support.

--- config/webpack.build.js
+++ config/webpack.build.js
@@ -2,6 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
 const commonConfig = require('./webpack.common.js');
 
 module.exports = webpackMerge(commonConfig, {
@@ -18,5 +20,12 @@ module.exports = webpackMerge(commonConfig, {
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin(),
     new ExtractTextPlugin('[name].[hash].css'),
+    new CompressionPlugin({
+        asset: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.js$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8
+    })
   ]
 });
